6f7bd8351523f846210e378ed49fb96243d1501c
Rohit Salunke <rohitsalunke107@gmail.com>
2026-04-19 14:30:18 +0530

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.get("/", (req, res) => {
   res.send("RealityForge API running 🚀")
 })
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
-})
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
